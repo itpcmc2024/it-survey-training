f0fb818235cb4b5683ff727426d37d8761a69567
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-02 16:00:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
   SYSTEM_NAME: "แบบสำรวจความต้องการอบรมด้านสารสนเทศ",
   VERSION: "1.0.0",
-
   LIFF_ID: "2010431720-qIfYB6de",
-
   GAS_URL: "https://script.google.com/macros/s/AKfycbwDdRafSi1t5iYoCAaa4HcDZ-Iil2DJcGjNQ1C4qXR0m4Q7ZvHFJwuUYmMCo3_5fff0JA/exec",
-
   GITHUB_URL: "https://itpcmc2024.github.io/it-survey-training/",
-
   LOGO_URL: "assets/logo.png",
-  BANNER_URL: "assets/banner.png"
+  BANNER_URL: "assets/banner.png",
+  ADMIN_PIN: "1234"
 };
