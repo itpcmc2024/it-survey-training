d0920356ddf09652e28743ad0f1d6de535d94828
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-15 10:23:23 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = Object.freeze({
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzIQjN_sAwEYexuhl4geFeaA1uEqIWTgw5YXTmFVA7VnzBs-XMlkM1e6177elaZykH_0w/exec',
+  GAS_WEB_APP_URL: 'PASTE_YOUR_GAS_WEB_APP_EXEC_URL_HERE',
   LIFF_ID: '2010431720-qlfYB6de',
   APP_TITLE: 'แบบสำรวจความต้องการพัฒนาทักษะด้านเทคโนโลยีดิจิทัล',
   REQUIRE_LIFF: true
